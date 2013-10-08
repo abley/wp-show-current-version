a384f343d0d5dd5b9ef98aa88b27920bdbdcc748
Dan Baley <dan.baley@gmail.com>
2013-10-07 18:13:40 -0700

    Set up initial plugin file

---
+++ plugin/show-current-version.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Show Current Version
+ * Plugin URI: https://github.com/abley/wp-show-current-version
+ * Description: Display the current WordPress version alongside the available update.
+ * Author: Dan Baley
+ * Version: 0.1-dev
+ * Author URI: https://github.com/abley
+ */
